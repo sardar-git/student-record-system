b2439b17a71021aba3194e3a0346e1e1ae18a692
Ganuga Sardar Vali <sardar.g713@gmail.com>
2026-05-16 16:57:04 +0530

    Added Student class

--- Student.java
+++ Student.java
@@ -1,1 +1,26 @@
 
+public class Student {
+
+    int id;
+    String name;
+    int age;
+    String course;
+
+    public Student(int id, String name, int age, String course) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    public void displayStudent() {
+
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+
+        System.out.println("-------------------------");
+    }
+}
